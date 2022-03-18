1fd7d053794967d7ed74124fcde325851ff40f15
Bonniebon <jan.vijfhuizen@outlook.com>
2022-03-18 15:17:28 +0100

    [Fix] Engine setup.

---
+++ VkEngine/WindowHandler.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "StringView.h"
+
+class WindowHandler final
+{
+public:
+	struct Info final
+	{
+		glm::ivec2 resolution{800, 600};
+		jlb::StringView stringView;
+	};
+
+	void Construct(const Info& info);
+	void Cleanup();
+
+	[[nodiscard]] VkSurfaceKHR CreateSurface(VkInstance instance);
+	void BeginFrame(bool& outQuit) const;
+	bool QueryHasResized();
+
+private:
+	GLFWwindow* _window = nullptr;
+	bool _resized = false;
+};
--- VkRenderer/Include/pch.h
+++ VkRenderer/Include/pch.h
@@ -1,2 +1,5 @@
 ﻿#pragma once
+#include "GLFW/glfw3.h"
+#include "vulkan/vulkan.h"
+#include "glm/glm.hpp"
 #include "Renderer.h"
