b99d57d7994d412590579e195859a375e536772f
Bonniebon <jan.vijfhuizen@outlook.com>
2022-05-04 21:02:44 +0200

    [Add] Stack allocator.

---
+++ JLB/Include/StackAllocator.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+
+namespace jlb
+{
+	class StackAllocator final
+	{
+	public:
+		struct CreateInfo final
+		{
+			size_t pageSize = 4096;
+			void* data = nullptr;
+		};
+
+		template <typename T>
+		struct Allocation final
+		{
+			T* ptr;
+			size_t id;
+			size_t* src;
+		};
+
+		void Allocate(const CreateInfo& info = {});
+		void Free() const;
+
+		[[nodiscard]] Allocation<void> Malloc(size_t size);
+		template <typename T>
+		void MFree(const Allocation<T>& allocation);
+
+		template <typename T>
+		[[nodiscard]] Allocation<T> New(size_t count = 1);
+
+	private:
+		StackAllocator* _next = nullptr;
+		size_t* _data = nullptr;
+		size_t _size;
+		size_t _current = 0;
+		size_t _id = 0;
+
+		[[nodiscard]] static size_t ToChunkSize(size_t size);
+	};
+
+	template <typename T>
+	void StackAllocator::MFree(const Allocation<T>& allocation)
+	{
+		if (_data != allocation.src)
+			return _next->MFree(allocation);
+		assert(_id == allocation.id + 1);
+
+		const size_t size = _data[_current - 1];
+		_current -= size + 1;
+		_id--;
+	}
+
+	template <typename T>
+	StackAllocator::Allocation<T> StackAllocator::New(const size_t count)
+	{
+		auto alloc = Malloc(sizeof(T) * count);
+		Allocation<T> allocation{};
+		allocation.id = alloc.id;
+		allocation.ptr = reinterpret_cast<T*>(alloc.ptr);
+		allocation.src = alloc.src;
+		return allocation;
+	}
+}
