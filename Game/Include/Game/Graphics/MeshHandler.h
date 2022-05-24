9575046a577e8013ef34b480fd8519ab543c2c5a
Bonniebon <jan.vijfhuizen@outlook.com>
2022-05-24 15:55:58 +0200

    [WIP] Generalizing mesh creation.

--- Game/Include/Game/Graphics/MeshHandler.h
+++ Game/Include/Game/Graphics/MeshHandler.h
@@ -7,11 +7,17 @@
 
 namespace game
 {
+	struct MeshBuffer final
+	{
+		VkBuffer buffer;
+		vk::MemBlock memBlock;
+	};
+
 	class MeshHandler final
 	{
 	public:
 		template <typename Type>
-		void CreateBuffer(const EngineOutData& outData, jlb::ArrayView<Type> data);
+		[[nodiscard]] static MeshBuffer CreateBuffer(const EngineOutData& outData, jlb::ArrayView<Type> data, VkBufferUsageFlags usageFlags);
 
 		template <typename Vertex, typename Index>
 		[[nodiscard]] static Mesh Create(const EngineOutData& outData, 
@@ -20,7 +26,7 @@ namespace game
 	};
 
 	template <typename Type>
-	void MeshHandler::CreateBuffer(const EngineOutData& outData, const jlb::ArrayView<Type> data)
+	MeshBuffer MeshHandler::CreateBuffer(const EngineOutData& outData, const jlb::ArrayView<Type> data, const VkBufferUsageFlags usageFlags)
 	{
 		auto& app = *outData.app;
 		auto& vkAllocator = *outData.vkAllocator;
@@ -34,6 +40,80 @@ namespace game
 		VkBuffer stagingBuffer;
 		auto result = vkCreateBuffer(app.logicalDevice, &bufferInfo, nullptr, &stagingBuffer);
 		assert(!result);
+
+		// Get buffer requirements.
+		VkMemoryRequirements stagingMemRequirements;
+		vkGetBufferMemoryRequirements(app.logicalDevice, stagingBuffer, &stagingMemRequirements);
+
+		// Define and allocate memory.
+		const uint32_t vertStagingBufferPoolId = vkAllocator.GetPoolId(app, stagingMemRequirements.memoryTypeBits,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+		const auto stagingBlock = vkAllocator.AllocateBlock(app, bufferInfo.size, stagingMemRequirements.alignment, vertStagingBufferPoolId);
+
+		result = vkBindBufferMemory(app.logicalDevice, stagingBuffer, stagingBlock.memory, stagingBlock.offset);
+		assert(!result);
+
+		// Move vertex/index data to a staging buffer.
+		void* stagingData;
+		vkMapMemory(app.logicalDevice, stagingBlock.memory, stagingBlock.offset, stagingBlock.size, 0, &stagingData);
+		memcpy(stagingData, static_cast<const void*>(data.data), bufferInfo.size);
+		vkUnmapMemory(app.logicalDevice, stagingBlock.memory);
+
+		bufferInfo.usage = usageFlags;
+
+		VkBuffer buffer;
+		result = vkCreateBuffer(app.logicalDevice, &bufferInfo, nullptr, &buffer);
+		assert(!result);
+
+		VkMemoryRequirements memRequirements;
+		vkGetBufferMemoryRequirements(app.logicalDevice, buffer, &memRequirements);
+
+		const uint32_t vertBufferPoolId = vkAllocator.GetPoolId(app, memRequirements.memoryTypeBits,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+		const auto memBlock = vkAllocator.AllocateBlock(app, bufferInfo.size, memRequirements.alignment, vertBufferPoolId);
+
+		result = vkBindBufferMemory(app.logicalDevice, buffer, memBlock.memory, memBlock.offset);
+		assert(!result);
+
+		VkCommandBuffer cmdBuffer;
+		auto cmdBufferAllocInfo = vk::CommandHandler::CreateBufferDefaultInfo(app);
+		result = vkAllocateCommandBuffers(app.logicalDevice, &cmdBufferAllocInfo, &cmdBuffer);
+		assert(!result);
+
+		VkFence fence;
+		auto fenceInfo = vk::SyncHandler::CreateFenceDefaultInfo();
+		result = vkCreateFence(app.logicalDevice, &fenceInfo, nullptr, &fence);
+		assert(!result);
+		result = vkResetFences(app.logicalDevice, 1, &fence);
+		assert(!result);
+
+		auto cmdBeginInfo = vk::CommandHandler::CreateBufferBeginDefaultInfo();
+		vkBeginCommandBuffer(cmdBuffer, &cmdBeginInfo);
+
+		VkBufferCopy region{};
+		region.srcOffset = 0;
+		region.dstOffset = 0;
+		region.size = memBlock.size;
+		vkCmdCopyBuffer(cmdBuffer, stagingBuffer, buffer, 1, &region);
+
+		result = vkEndCommandBuffer(cmdBuffer);
+		assert(!result);
+
+		auto cmdSubmitInfo = vk::CommandHandler::CreateSubmitDefaultInfo(cmdBuffer);
+		result = vkQueueSubmit(app.queues.graphics, 1, &cmdSubmitInfo, fence);
+		assert(!result);
+
+		result = vkWaitForFences(app.logicalDevice, 1, &fence, VK_TRUE, UINT64_MAX);
+		assert(!result);
+
+		vkDestroyFence(app.logicalDevice, fence, nullptr);
+		vkDestroyBuffer(app.logicalDevice, stagingBuffer, nullptr);
+		vkAllocator.FreeBlock(stagingBlock);
+
+		MeshBuffer meshBuffer{};
+		meshBuffer.buffer = buffer;
+		meshBuffer.memBlock = memBlock;
+		return meshBuffer;
 	}
 
 	template <typename Vertex, typename Index>
