32cf2c850121cab492cb8693de79fd6e12dbfed6
Jan <jan.vijfhuizen@outlook.com>
2022-07-20 10:36:36 +0200

    [Change] Task system now can be manually cleared.

---
+++ Game/Include/Game/Systems/CollisionSystem.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "VkEngine/Systems/TaskSystem.h"
+
+namespace game
+{
+	struct CollisionTask final
+	{
+		
+	};
+
+	class CollisionSystem final : public vke::TaskSystem<CollisionTask>
+	{
+		
+	};
+}
