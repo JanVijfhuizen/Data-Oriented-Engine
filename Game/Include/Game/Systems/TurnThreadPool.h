d4dfe60af867a39c21ddf29600b63294fb8f554e
Jan <jan.vijfhuizen@outlook.com>
2022-07-20 12:17:59 +0200

    [Add] TurnThreadPool setup.

---
+++ Game/Include/Game/Systems/TurnThreadPool.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "VkEngine/Systems/TaskSystem.h"
+
+namespace game
+{
+	struct TurnThreadPoolTask final
+	{
+		void (*func)(const vke::EngineData& info, jlb::Systems<vke::EngineData> systems, void* userPtr);
+		void* userPtr = nullptr;
+	};
+
+	class TurnThreadPool final : public vke::TaskSystem<TurnThreadPoolTask>
+	{
+		bool _takesTasks = false;
+
+		void OnPreUpdate(const vke::EngineData& info, jlb::Systems<vke::EngineData> systems, const jlb::NestedVector<TurnThreadPoolTask>& tasks) override;
+		[[nodiscard]] bool AutoClearOnFrameEnd() override;
+		bool ValidateOnTryAdd(const TurnThreadPoolTask& task) override;
+	};
+}
---
+++ Game/Source/Systems/TurnThreadPool.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "Systems/TurnThreadPool.h"
+#include "Systems/TurnSystem.h"
+
+namespace game
+{
+	void TurnThreadPool::OnPreUpdate(const vke::EngineData& info, 
+		const jlb::Systems<vke::EngineData> systems,
+		const jlb::NestedVector<TurnThreadPoolTask>& tasks)
+	{
+		TaskSystem<TurnThreadPoolTask>::OnPreUpdate(info, systems, tasks);
+
+		const auto turnSys = systems.GetSystem<TurnSystem>();
+		const bool isTickEvent = turnSys->GetIfTickEvent();
+		_takesTasks = isTickEvent;
+		ClearTasks();
+	}
+
+	bool TurnThreadPool::AutoClearOnFrameEnd()
+	{
+		return false;
+	}
+
+	bool TurnThreadPool::ValidateOnTryAdd(const TurnThreadPoolTask& task)
+	{
+		return _takesTasks ? TaskSystem<TurnThreadPoolTask>::ValidateOnTryAdd(task) : false;
+	}
+}
