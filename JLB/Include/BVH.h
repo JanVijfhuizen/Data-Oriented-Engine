fd2f664b1927208d70a8b5ec650c120a2aafcea0
Jan <jan.vijfhuizen@outlook.com>
2022-07-20 19:37:02 +0200

    [Add] BVH node creation.

--- JLB/Include/BVH.h
+++ JLB/Include/BVH.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <glm/vec2.hpp>
-#include "Array.h"
+
+#include "JlbMath.h"
+#include "Vector.h"
 #include "StackAllocator.h"
 
 namespace jlb
@@ -14,26 +16,26 @@ namespace jlb
 			glm::vec2 scale;
 		};
 
-		void Allocate(StackAllocator& allocator, ArrayView<Instance> instances);
+		void Allocate(StackAllocator& allocator, ArrayView<Instance> instances, size_t nodeCapacity = 4);
 		void Free(StackAllocator& allocator);
 
 	private:
 		struct Node final
 		{
-			glm::vec2 rTop{};
-			glm::vec2 lBot{};
+			glm::vec2 lTop{};
+			glm::vec2 rBot{};
 			uint32_t children[2]{0, 0};
 			uint32_t begin = 0;
 			uint32_t end = 0;
 		};
 
-		Array<Node> _nodes{};
+		Vector<Node> _nodes{};
 		Array<uint32_t> _indexes{};
 
-		void* QuickSort(const Instance* instances, uint32_t from, uint32_t to, uint32_t depth);
+		uint32_t QuickSort(const Instance* instances, uint32_t from, uint32_t to, uint32_t nodeCapacity, uint32_t depth);
 	};
 
-	inline void BoundingVolumeHierarchy::Allocate(StackAllocator& allocator, const ArrayView<Instance> instances)
+	inline void BoundingVolumeHierarchy::Allocate(StackAllocator& allocator, const ArrayView<Instance> instances, const size_t nodeCapacity)
 	{
 		const auto& length = instances.length;
 		_nodes.Allocate(allocator, length);
@@ -42,7 +44,7 @@ namespace jlb
 		for (uint32_t i = 0; i < length; ++i)
 			_indexes[i] = i;
 
-		QuickSort(instances.data, 0, length, 0);
+		QuickSort(instances.data, 0, length, nodeCapacity, 0);
 	}
 
 	inline void BoundingVolumeHierarchy::Free(StackAllocator& allocator)
@@ -51,13 +53,27 @@ namespace jlb
 		_nodes.Free(allocator);
 	}
 
-	inline void* BoundingVolumeHierarchy::QuickSort(
-		const Instance* instances, const uint32_t from, const uint32_t to, const uint32_t depth)
+	inline uint32_t BoundingVolumeHierarchy::QuickSort(
+		const Instance* instances, const uint32_t from, const uint32_t to, const uint32_t nodeCapacity, const uint32_t depth)
 	{
-		// Calculate median.
+		// Calculate median and bounds.
 		glm::vec2 median{};
-		for (uint32_t i = from; i < to; ++i)
-			median += instances[_indexes[i]].position;
+		glm::vec2 lTop{};
+		glm::vec2 rBot{};
+
+		for (uint32_t i = from; i < to; ++i) 
+		{
+			const auto& instance = instances[_indexes[i]];
+			const auto& position = instance.position;
+			const auto scale = instance.scale * .5f;
+
+			median += position;
+			lTop.x = math::Min(lTop.x, position.x - scale.x);
+			lTop.y = math::Max(lTop.y, position.y + scale.y);
+			rBot.x = math::Max(rBot.x, position.x + scale.x);
+			rBot.y = math::Min(rBot.y, position.y - scale.x);
+		}
+
 		median /= to - from;
 
 		uint32_t partitionIndex = to;
@@ -80,8 +96,15 @@ namespace jlb
 			i += left;
 		}
 
-		from + 1 >= partitionIndex ? nullptr : QuickSort(instances, from, partitionIndex, depth + 1);
-		partitionIndex + 1 >= to ? nullptr : QuickSort(instances, partitionIndex, to, depth + 1);
-		return nullptr;
+		const uint32_t index = _nodes.GetCount();
+		auto& node = _nodes.Add();
+		node.lTop = lTop;
+		node.rBot = rBot;
+		node.begin = from;
+		node.end = to;
+		node.children[0] = from + nodeCapacity >= partitionIndex ? 0 : QuickSort(instances, from, partitionIndex, nodeCapacity, depth + 1);
+		node.children[1] = partitionIndex + nodeCapacity >= to ? 0 : QuickSort(instances, partitionIndex, to, nodeCapacity, depth + 1);
+
+		return index;
 	}
 }
