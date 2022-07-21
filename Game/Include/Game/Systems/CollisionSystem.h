147fec3b9bd5018d89ac7cff01115ed50c75b749
Jan <jan.vijfhuizen@outlook.com>
2022-07-21 16:03:23 +0200

    [Change] CollisionSystem order.
    Now the user can get the turn data in preupdate.

--- Game/Include/Game/Systems/CollisionSystem.h
+++ Game/Include/Game/Systems/CollisionSystem.h
@@ -12,7 +12,7 @@ namespace game
 
 		void Allocate(const vke::EngineData& info) override;
 		void Free(const vke::EngineData& info) override;
-		void OnPreUpdate(const vke::EngineData& info, jlb::Systems<vke::EngineData> systems,
+		void OnUpdate(const vke::EngineData& info, jlb::Systems<vke::EngineData> systems,
 			const jlb::NestedVector<CollisionTask>& tasks) override;
 		[[nodiscard]] size_t DefineCapacity(const vke::EngineData& info) override;
 		[[nodiscard]]size_t DefineNestedCapacity(const vke::EngineData& info) override;
