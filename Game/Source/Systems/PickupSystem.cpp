1cdb751544f9c5c6f6dbc3e72f374b3411960c2b
Jan <jan.vijfhuizen@outlook.com>
2022-08-16 19:32:28 +0200

    [WIP] Adding hands.

--- Game/Source/Systems/PickupSystem.cpp
+++ Game/Source/Systems/PickupSystem.cpp
@@ -1,5 +1,8 @@
 ﻿#include "pch.h"
 #include "Systems/PickupSystem.h"
+
+#include "Curve.h"
+#include "JlbMath.h"
 #include "Systems/EntitySystem.h"
 #include "Systems/TurnSystem.h"
 #include "VkEngine/Systems/ThreadPoolSystem.h"
@@ -18,10 +21,33 @@ namespace game
 		threadTask.func = [](const vke::EngineData& info, const jlb::Systems<vke::EngineData> systems, void* userPtr)
 		{
 			const auto self = static_cast<PickupSystem*>(userPtr);
+			const auto turnSys = systems.GetSystem<TurnSystem>();
+
+			const auto& tasks = self->GetTasks();
+			auto curveOvershoot = jlb::CreateCurveOvershooting();
+			auto& tasksOutput = self->GetOutputEditable();
+			auto& dumpAllocator = *info.dumpAllocator;
+
+			const float lerp = turnSys->GetTickLerp();
+			const float eval = jlb::DoubleCurveEvaluate(lerp, curveOvershoot, curveOvershoot);
+
+			for (auto& task : tasks)
+			{
+				const auto pos = jlb::math::LerpPct(task._position, task._pickupPosition, eval);
+
+				PickupTaskOutput output{};
+				output.lHandPosition = pos;
+				output.rHandPosition = pos;
+				tasksOutput.Add(dumpAllocator, output);
+			}
 		};
 
 		threadTask.userPtr = this;
-		
+
+		auto& tasksOutput = GetOutputEditable();
+		auto& dumpAllocator = *info.dumpAllocator;
+		tasksOutput.PreAllocateNested(dumpAllocator, GetCount());
+
 		const auto threadSys = systems.GetSystem<vke::ThreadPoolSystem>();
 		const auto result = threadSys->TryAdd(info, threadTask);
 		assert(result != SIZE_MAX);
