5a5bc25443d2f49c360a19a5ce9abdadbfa3b974
Jan <jan.vijfhuizen@outlook.com>
2022-08-17 19:12:15 +0200

    [Refactor] Threadable task system w/ output.

--- Game/Include/Game/Systems/MovementSystem.h
+++ Game/Include/Game/Systems/MovementSystem.h
@@ -1,13 +1,13 @@
 ﻿#pragma once
+#include "ThreadedTaskSystemWithOutput.h"
 #include "Components/MovementComponent.h"
-#include "VkEngine/Systems/TaskSystemWithOutput.h"
 
 namespace game
 {
 	typedef MovementComponent MovementTask;
 
 	// Handles the movement of characters. Also handles the corresponding animation (by scaling) and rotations.
-	class MovementSystem final : public vke::TaskSystemWithOutput<MovementTask, MovementTask>
+	class MovementSystem final : public ThreadedTaskSystemWithOutput<MovementSystem, MovementTask, MovementTask>
 	{
 	public:
 		// How many times bobbing happens during a tick.
@@ -20,8 +20,6 @@ namespace game
 	private:
 		[[nodiscard]] size_t DefineCapacity(const vke::EngineData& info) override;
 		[[nodiscard]] bool ValidateOnTryAdd(const MovementTask& task) override;
-
-		void OnPreUpdate(const vke::EngineData& info, jlb::Systems<vke::EngineData> systems,
-			const jlb::NestedVector<MovementTask>& tasks) override;
+		[[nodiscard]] threadableFunc DefineThreadable() override;
 	};
 }
